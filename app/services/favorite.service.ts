16558de2ab92e12eab502ec1b370fcb9b71e4f66
Philippe Doreau <p.doreau@criteo.com>
2018-01-09 12:18:13 +0100

    Storage with Couchbase

--- app/services/favorite.service.ts
+++ app/services/favorite.service.ts
@@ -3,14 +3,24 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { CouchbaseService } from './couchbase.service';
 
 @Injectable()
 export class FavoriteService {
 
     favorites: Array<number>;
+    docId: string = "favorites";
 
-    constructor(private dishService: DishService){
+    constructor(private dishService: DishService, private couchbaseservice : CouchbaseService){
         this.favorites = [];
+
+        let doc = this.couchbaseservice.getDocument(this.docId);
+        if (doc == null ) {
+            this.couchbaseservice.createDocument({"favorites": [] }, this.docId);
+        } 
+        else{
+            this.favorites = doc.favorites;
+        }
     }
 
     isFavorite(id: number): boolean{
@@ -20,6 +30,7 @@ export class FavoriteService {
     addFavorite(id: number): boolean {
         if(!this.isFavorite(id)){
             this.favorites.push(id);
+            this.couchbaseservice.updateDocument(this.docId, {"favorites": this.favorites});
         }
         return true;
     }
@@ -33,6 +44,7 @@ export class FavoriteService {
         let index = this.favorites.indexOf(id);
         if(index >= 0){
             this.favorites.splice(index,1);
+            this.couchbaseservice.updateDocument(this.docId, {"favorites": this.favorites});
             return this.getFavorites();
         }
         else{
